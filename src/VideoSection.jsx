c61a1089309be469d533a3f9061c286984b5927b
mohammed irfan <irfancodes1@gmail.com>
2025-01-14 11:00:29 +0530

    added videos in the public folder

--- src/VideoSection.jsx
+++ src/VideoSection.jsx
@@ -1,16 +1,19 @@
 
 
 import React, { useEffect, useRef } from "react";
-import Ad1 from "./assets/Ad1.mp4";
-import Ad2 from "./assets/Ad2.mp4";
-import Ad3 from "./assets/Ad3.mp4";
-import Ad4 from "./assets/Ad4.mp4";
-import Ad5 from "./assets/Ad5.mp4";
-import Ad6 from "./assets/Ad6.mp4";
+
 
 const VideoSection = () => {
   const videoRefs = useRef([]);
-  
+  const videos = [
+    "/videos/Ad1.mp4",
+    "/videos/Ad2.mp4",
+    "/videos/Ad3.mp4",
+    "/videos/Ad4.mp4",
+    "/videos/Ad5.mp4",
+    "/videos/Ad6.mp4",
+  ];
+
   const videoTitles = [
     "Ad Edits",
     "Gym Edits",
@@ -59,7 +62,7 @@ const VideoSection = () => {
           Recent Work of My Clients
         </h2>
         <div className="max-w-5xl mx-auto">
-          {[Ad1, Ad2, Ad3, Ad4, Ad5, Ad6]
+          {videos
             .reduce((rows, src, index) => {
               if (index % 3 === 0) {
                 rows.push([]);
@@ -72,7 +75,6 @@ const VideoSection = () => {
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 sm:gap-6 md:gap-8">
                   {row.map(({ src, title }, index) => (
                     <div key={index} className="flex flex-col">
-                      {/* Video Container with fixed height */}
                       <div className="rounded-2xl overflow-hidden relative border-2 border-customTeal2 h-full">
                         <video
                           ref={(el) => (videoRefs.current[rowIndex * 3 + index] = el)}
@@ -85,7 +87,6 @@ const VideoSection = () => {
                           controlsList="nodownload noplaybackrate"
                         />
                       </div>
-                      {/* Title below video */}
                       <h3 className="text-customTeal2 text-2xl font-extrabold text-stroke mt-2 text-center">
                         {title}
                       </h3>
@@ -97,6 +98,7 @@ const VideoSection = () => {
                 )}
               </React.Fragment>
             ))}
+
         </div>
       </div>
     </section>
